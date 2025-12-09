f9c8b5a902f50ddefe9515c8b8b905ca90add665
Fermin <31296603+SandyPlatano@users.noreply.github.com>
2025-12-09 13:52:56 -0500

    Add Sentry usage guide and configure Sentry DSN

--- nextjs/sentry.server.config.ts
+++ nextjs/sentry.server.config.ts
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN || "https://a45ae48506221fece9fc263ac4dd8b8d@o4510506408214528.ingest.us.sentry.io/4510506414768128",
   
   // Adjust this value in production, or use tracesSampler for finer control
   tracesSampleRate: 1.0,
